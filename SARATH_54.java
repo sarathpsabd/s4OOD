281ea1f6db3b697d05c778c59a24352c5eaf873d
sarathpsabd <35604162+sarathpsabd@users.noreply.github.com>
2019-02-10 20:52:06 +0530

    Add files via upload

---
+++ SARATH_54.java
@@ -0,0 +1,12 @@
+public class L {
+	public static void main(String[] args) {
+		int i;
+    		for(i=0;i<5;i++){
+      			System.out.println("1");
+     		}
+    		for(i=0;i<8;i++){
+      			System.out.print("1");
+   		}
+    
+	}
+}
